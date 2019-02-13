f6be97c165eafacd3fd2b7297371593d9f21ff82
mlugo2 <marcolugo02@gmail.com>
2019-02-13 02:56:11 -0600

    Organizing folders

---
+++ src/Engine/VertexBuffer.js
@@ -0,0 +1,39 @@
+"use strict";   // Operate in Strict mode
+
+var gEngine = gEngine || {};
+
+// The VertexBuffer object
+gEngine.VertexBuffer = (function() {
+  // First: define the vertices for a square
+  var verticesOfSquare = [
+    0.5, 0.5, 0.0,
+    -0.5, 0.5, 0.0,
+    0.5, -0.5, 0.0,
+    -0.5, -0.5, 0.0
+  ];
+
+  // reference to the vertex position for the square in the gl context
+  var mSquareVertexBuffer = null;
+
+  var getGLVertexRef = function() { return mSquareVertexBuffer; }
+
+  var initialize = function() {
+    var gl = gEngine.Core.getGL();
+
+    // Step A: Create a buffer on the gGL context for our vertex position
+    mSquareVertexBuffer = gl.createBuffer();
+
+    // Step B: Active vertexBuffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, mSquareVertexBuffer);
+
+    // Step C: Loads verticesOfSquare into the vertexBuffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticesOfSquare),
+      gl.STATIC_DRAW);
+  };
+
+  var mPublic = {
+    initialize: initialize,
+    getGLVertexRef: getGLVertexRef
+  };
+  return mPublic;
+}());
---
+++ src/Game/Game.js
@@ -0,0 +1,18 @@
+function Game(htmlCanvasID) {
+  // The game for drawing
+  this.mShader = null;
+
+  gEngine.Core.initializeWebGL(htmlCanvasID);
+
+  this.mShader = new SimpleShader(
+    "src/GLSLShaders/SimpleVS.glsl",
+    "src/GLSLShaders/WhiteFS.glsl");
+
+  // Step C1: Clear the canvas
+  gEngine.Core.clearCanvas([0, 0.8, 0, 1]);
+  // Step C2: Activate the proper shader
+  this.mShader.activateShader();
+  // Step C3: Draw with the currently activate geometry and the activated shader
+  var gl = gEngine.Core.getGL();
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
