51d03d049ea63cee4946d372dd8174fcc2000119
mlugo2 <marcolugo02@gmail.com>
2019-02-17 04:37:11 -0600

    Encapsulated the transform operator!!!

---
+++ src/Engine/Transform.js
@@ -0,0 +1,71 @@
+function Transform() {
+  this.mPosition = vec2.fromValues(0, 0); // translation operator
+  this.mScale = vec2.fromValues(1, 1);    // Scaling operator
+  this.mRotationInRad = 0.0;              // Rotation in radians!
+}
+
+// Position getters and setters
+Transform.prototype.setPosition = function (xPos, yPos) { this.setXPos(xPos); this.setYPos(yPos); };
+Transform.prototype.getPosition = function () { return this.mPosition; };
+Transform.prototype.getXPos = function () { return this.mPosition[0]; };
+Transform.prototype.setXPos = function (xPos) { this.mPosition[0] = xPos; };
+Transform.prototype.incXPosBy = function (delta) { this.mPosition[0] += delta; };
+Transform.prototype.getYPos = function () { return this.mPosition[1]; };
+Transform.prototype.setYPos = function (yPos) { this.mPosition[1] = yPos; };
+Transform.prototype.incYPosBy = function (delta) { this.mPosition[1] += delta; };
+
+// Size getters and setters
+Transform.prototype.setSize = function (width, height) {
+  this.setWidth(width);
+  this.setHeight(height);
+};
+
+Transform.prototype.getSize = function () { return this.mScale; };
+Transform.prototype.incSizeBy = function (delta) {
+  this.incWidthBy(delta);
+  this.incHeightBy(delta);
+};
+
+Transform.prototype.getWidth = function () { return this.mScale[0]; };
+Transform.prototype.setWidth = function (width) { this.mScale[0] = width; };
+Transform.prototype.incWidthBy = function (delta) { this.mScale[0] += delta; };
+Transform.prototype.getHeight = function () { return this.mScale[1]; };
+Transform.prototype.setHeight = function (height) { this.mScale[1] = height; };
+Transform.prototype.incHeightBy = function (delta) { this.mScale[1] += delta; };
+
+// rotation getters and setters
+Transform.prototype.setRotationInRad = function (rotation) {
+  this.mRotationInRad = rotation;
+  while (this.mRotationInRad > (2 * Math.PI)) {
+    this.mRotationInRad -= (2 * Math.PI);
+  }
+};
+
+Transform.prototype.setRotationInDegree = function (rotation) {
+  this.setRotationInRad(rotation * Math.PI / 180.0);
+};
+
+Transform.prototype.incRotationByDegree = function (deltaDegree) {
+  this.incRotationByRad(deltaDegree * Math.PI / 180.0);
+};
+
+Transform.prototype.incRotationByRad = function (deltaRad) {
+  this.setRotationInRad(this.mRotationInRad + deltaRad);
+};
+
+Transform.prototype.getRotationInRad = function () { return this.mRotationInRad; };
+Transform.prototype.getRotationInDegree = function () { return this.mRotationInRad * 180.0 / Math.PI; };
+
+Transform.prototype.getXform = function () {
+  // Creates a blank identity matrix
+  var matrix = mat4.create();
+
+  // Step 1: compute translation, for now z is always at 0.0
+  mat4.translate(matrix, matrix, vec3.fromValues(this.getXPos(), this.getYPos(), 0.0));
+  // Step 2: concatenate with rotation.
+  mat4.rotateZ(matrix, matrix, this.getRotationInRad());
+  // Step 3: concatenate with scaling
+  mat4.scale(matrix, matrix, vec3.fromValues(this.getWidth(), this.getHeight(), 1.0));
+
+  return matrix;
+};
--- src/Game/Game.js
+++ src/Game/Game.js
@@ -17,23 +17,20 @@ function Game(htmlCanvasID) {
   // Step D: Draw!
   gEngine.Core.clearCanvas([0, 0.8, 0, 1]); // Clear the canvas
 
-  // create a new identify transform operator
-  var xform = mat4.create();  // WARNING: this is breakikng the code!
+  // Step E: sets the white Renderable object's transform
+  this.mWhiteSq.getXform().setPosition(-0.25, 0.25);
+  this.mWhiteSq.getXform().setRotationInRad(0.2); // in Radians
+  this.mWhiteSq.getXform().setSize(1.2, 1.2);
 
-  // Step E: compute the white square transform
-  mat4.translate(xform, xform, vec3.fromValues(-0.25, 0.25, 0.0));
-  mat4.rotateZ(xform, xform, 0.2);
-  mat4.scale(xform, xform, vec3.fromValues(1.2, 1.2, 1.0));
+  // Step F: draws the white square (transform behavior in the object)
+  this.mWhiteSq.draw();
 
-  // Step F: draw the white square with the computed transform
-  this.mWhiteSq.draw(xform);
-
-  // Step G: compute the red square transform
-  mat4.identity(xform); // restart
-  mat4.translate(xform, xform, vec3.fromValues(0.25, -0.25, 0.0));
-  mat4.rotateZ(xform, xform, -0.785);
-  mat4.scale(xform, xform, vec3.fromValues(0.4, 0.4, 1.0));
-
-  // Step H: draw the red square with the computed transform
-  this.mRedSq.draw(xform);
+  // Step G: sets and red square transform
+  this.mRedSq.getXform().setXPos(0.25);
+  this.mRedSq.getXform().setYPos(-0.25);
+  this.mRedSq.getXform().setRotationInDegree(45);
+  this.mRedSq.getXform().setWidth(0.4);
+  this.mRedSq.getXform().setHeight(0.4);
+  // Step H: draw the red square (transform in the object)
+  this.mRedSq.draw();
 }
